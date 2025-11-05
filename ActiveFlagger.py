22ff24d084afddd755bc81fa3441bd687697ab4a
AJAK404 <141361396+AJAK404@users.noreply.github.com>
2025-11-05 13:38:27 -0800

    Update ActiveFlagger.py

--- ActiveFlagger.py
+++ ActiveFlagger.py
@@ -10,9 +10,9 @@ def activeflag(data, cal, lowo = -5, higho = 0,lows = -5, highs = 0, highl = -30
                highd = -5, highr = -5, higha = -5, highal = -30, highn = -30):
   # Checks normalacy as data is recorded after required amount of data; is either whole or scrolling.
   flags = {}
-  vnao = np.mean(20 * np.log10(np.abs(cal["VNAO"])))
-  vnas = np.mean(20* np.log10(np.abs(cal["VNAS"])))
-  vnal = np.mean(20* np.log10(np.abs(cal["VNAL"])))
+  vnao = mlin(cal["VNAO"])
+  vnas = mlin(cal["VNAS"])
+  vnal = mlin(cal["VNAL"])
   good = True
   if lowo > vnao:
       good = False
@@ -26,13 +26,13 @@ def activeflag(data, cal, lowo = -5, higho = 0,lows = -5, highs = 0, highl = -30
       good = False
   flags.update({"cal": good})
   if len(data) == 1:
-    rec = np.mean(20* np.log10(np.abs(data["rec"])))
+    rec = mlin(data["rec"])
     rnorm = bool(highr > rec)
     flags.update({"rec": rnorm})
   else:
-    ante = np.mean(20* np.log10(np.abs(data["ant"])))
-    load = np.mean(20* np.log10(np.abs(data["load"])))
-    loud = np.mean(20* np.log10(np.abs(data["noise"])))
+    ante = mlin(data["ant"])
+    load = mlin(data["load"])
+    loud = mlin(data["noise"])
     anorm = bool(higha > ante)
     alnorm = bool(highal > load)
     nnorm = bool(highn > loud)
