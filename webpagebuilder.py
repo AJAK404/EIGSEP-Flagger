7827a728a4e196aee7171f8f874bc8374aef387d
AJ Rhee <141361396+AJAK404@users.noreply.github.com>
2026-03-04 14:27:35 -0800

    Update webpagebuilder.py

--- webpagebuilder.py
+++ webpagebuilder.py
@@ -125,7 +125,7 @@ class Website:
   
   @classmethod
   def seespec(cls, Chaos=False):
-    print(cls.spec["0"])
+    #print(cls.spec["0"])
     plt.figure()
     for k in ["0", "02", "04", "1", "13", "15", "2", "24", "3", "35", "4", "5"]:
       plt.plot(np.log10(np.abs(cls.spec[k][0])), label=k)
