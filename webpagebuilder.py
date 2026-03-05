5b0eafe5a52d0039a44d3de7c694a9b90b59ee2d
AJ Rhee <141361396+AJAK404@users.noreply.github.com>
2026-03-04 16:46:43 -0800

    Update webpagebuilder.py

--- webpagebuilder.py
+++ webpagebuilder.py
@@ -31,8 +31,8 @@ class Website:
            {"A_status": "error", "B_status": "error", "A_timestamp":0, "A_T_now":0, "B_timestamp":0, "B_T_now":0}, {"distance_m": 0}, 
            {"az_pos": 0, "el_pos": 0}, {"sw_state":0}] # Contains metadata; this empty placeholder avoids keyerrors. 
   ks = ["0", "02", "04", "1", "13", "15", "2", "24", "3", "35", "4", "5"] # A list of all possible spectra.
-  spec={"0": np.array([0]), "02": np.array([0]), "04": np.array([0]), "1": np.array([0]), "13": np.array([0]), "15": np.array([0]), 
-        "2": np.array([0]), "24": np.array([0]), "3": np.array([0]), "35": np.array([0]), "4": np.array([0]), "5": np.array([0])}
+  spec={"0": np.array([[0]]), "02": np.array([[0]]), "04": np.array([[0]]), "1": np.array([[0]]), "13": np.array([[0]]), "15": np.array([[0]]), 
+        "2": np.array([[0]]), "24": np.array([[0]]), "3": np.array([[0]]), "35": np.array([[0]]), "4": np.array([[0]]), "5": np.array([[0]])}
   # Contains graphable spectrum data; this empty placeholder avoids keyerrors.
   freqs = [0]
   
