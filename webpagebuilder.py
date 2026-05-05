13ffdaa58d58ba8e45caf8fbaa8830de45aecc26
AJ Rhee <141361396+AJAK404@users.noreply.github.com>
2026-05-04 23:21:23 -0700

    Update webpagebuilder.py
    DOCUMENTATION!!!!

--- webpagebuilder.py
+++ webpagebuilder.py
@@ -34,7 +34,7 @@ class Website:
   data = {"ant": [0], "load": [0], "noise": [0], "rec": [0]} # Contains the S11 data; this empty placeholder avouds keyerrors.
   cal = {"VNAO": [0], "VNAS": [0], "VNAL": [0]} # Contains the S11 calibration data; this empty placeholder avoids keyerrors.
   opene = False # Denotes whether the website has been opened or not.
-  mlist = [[0], [0], ["xxx"], 
+  mlist = [[0], [0], {"pot_el_voltage": 0, "pot_az_voltage":0}, 
            {"A_status": "error", "B_status": "error", "A_timestamp":0, "A_T_now":0, "B_timestamp":0, "B_T_now":0, "A_T_target": 0, "B_T_target": 0}, {"distance_m": 0}, 
            {"az_pos": 0, "el_pos": 0}, {"sw_state":0}] # Contains metadata; this empty placeholder avoids keyerrors. 
   ks = ["0", "02", "04", "1", "13", "15", "2", "24", "3", "35", "4", "5"] # A list of all possible spectra.
@@ -60,12 +60,11 @@ class Website:
   spew = True # Denotes whether spectra data is being collected.
 
   @classmethod
-  def bin2string(cls, k):
+  def bin2string(cls, k): # Converts the binary to a state using the dictionary from lines 46-56.
     for l in cls.path_str:
       if cls.path_str[l] == k:
         return l
     return "idk"
-
   
   @classmethod
   def lin(cls, x): # Linearizes the datapoints in x.
@@ -100,22 +99,22 @@ class Website:
 
   @classmethod
   def grabs11(cls): # Grabs S11 data on the S11 thread.
-    if cls.r != None:
+    if cls.r != None: # Checks if redis can be fetched.
       while cls.flag: # Stops when the program ends.
         try:
           d, c, h, m = cls.r.read_vna_data() # Gets data, cal, header, and metadata.
           cls.data = d
           cls.cal = c
-          cls.s11time = str(datetime.fromtimestamp(m["temp_ctrl"]["A_timestamp"]))
+          cls.s11time = str(datetime.fromtimestamp(m["temp_ctrl"]["A_timestamp"])) # Gets a time.
           cls.s11w = True
         except:
           cls.s11w = False
     else:
-      cls.s11w = False
+      cls.s11w = False # Notes if unable to get s11 data.
   
   @classmethod
   def grabbit(cls): # Grabs metadata on the metadata thread.
-    if cls.r != None:
+    if cls.r != None: # Checks if there's a redis.
       while cls.flag:
         try:
           meta = cls.r.get_live_metadata()
@@ -129,7 +128,7 @@ class Website:
                                             [[tec["A_timestamp"]], [tec["A_T_now"]]], 
                                             [[tec["B_timestamp"]], [tec["B_T_now"]]]], axis = 2)
           # Adds a datapoint of format (datetime, temperature) to the temperature array.
-          cls.mlist = [meta["imu_antenna"], meta["imu_panda"], ["xxx"], meta["tempctrl"], meta["lidar"], meta["motor"], meta["rfswitch"]]
+          cls.mlist = [meta["imu_antenna"], meta["imu_panda"], meta["potmon"], meta["tempctrl"], meta["lidar"], meta["motor"], meta["rfswitch"]]
           time.sleep(.1) # Limits rate of metadata grabbing.
           #print("Metadata in grabbing thread: \n" + str(cls.mlist))
         except:
@@ -140,7 +139,7 @@ class Website:
   @classmethod
   def grabbe(cls): # Get each part of metadata for the website in a conveinet manner.
     #print("Metadata in main thread to page: \n" + str(cls.mlist))
-    return cls.mlist[0], cls.mlist[1], cls.mlist[3], cls.mlist[4], cls.mlist[5], cls.mlist[6],
+    return cls.mlist[0], cls.mlist[1], cls.mlist[2], cls.mlist[3], cls.mlist[4], cls.mlist[5], cls.mlist[6],
   
   @classmethod
   def seetemp(cls): # Graphs the temperature.
@@ -149,7 +148,7 @@ class Website:
     else: # If 1200 temperatures do not exist, graphs what is there.
       x = 0
     plt.figure(figsize=(6.4,3.5))
-    if cls.mlist[3]["A_status"] != "error":
+    if cls.mlist[3]["A_status"] != "error": # Only plots when there's no error.
       atc = cls.tdata[2][cls.tdata[2][x:, 0].argsort()]
       plt.scatter(atc[0][1:], atc[1][1:], color = "green",label = "A_Temp_Ctrl")
     if cls.mlist[3]["B_status"] != "error":
@@ -171,7 +170,7 @@ class Website:
     return img64
   
   @classmethod
-  def seespec(cls):
+  def seespec(cls): # Plots spectral data.
     plt.figure(figsize=(6.4,3.5))
     #print(len(cls.freqs))
     #print(len(np.log10(np.abs(cls.spec["1"][0]))))
@@ -183,7 +182,7 @@ class Website:
     plt.xlabel("Frequency (MHz)")
     plt.ylabel("Power (log(counts))")
     plt.tight_layout()
-    buffer = io.BytesIO()
+    buffer = io.BytesIO() # Converts image of the plot into a string.
     plt.savefig(buffer, format='png')
     buffer.seek(0)
     img64 = base64.b64encode(buffer.read()).decode('utf-8')
@@ -191,21 +190,21 @@ class Website:
     return img64
 
   @classmethod
-  def seespectrum(cls, ks):
-    if cls.r2 != None:
+  def seespectrum(cls, ks): # Grabs spectral data.
+    if cls.r2 != None: # Checks if there's a redis.
       while cls.flag:
         try:
           cls.readspec = cls.r2.read_corr_data(timeout = 10)
           header = cls.r2.get_corr_header()
-          cls.freqs, dfreq = calc_freqs_dfreq(header["sample_rate"], header["nchan"])
-          cls.spec = eo.io.reshape_data(cls.readspec[2])
+          cls.freqs, dfreq = calc_freqs_dfreq(header["sample_rate"], header["nchan"]) # Gets the spectral frequecies.
+          cls.spec = eo.io.reshape_data(cls.readspec[2]) # Gets the spectral data.
           cls.spew = True
         except:
           cls.spew = False
     else:
-      cls.spew = False
+      cls.spew = False # Notes when unable to obtain spectral data.
   
-  def ripper(fname):
+  def ripper(fname): # Rips up the input filename so the webpage can be named after it.
     i= -4
     chara = fname[i]
     fn = ""
@@ -218,23 +217,23 @@ class Website:
         return fn
     return fn
 
-  def good(a):
+  def good(a): # Silly little thing that coverts boolean to a string.
     if a:
       return "Normal"
     else:
       return "Abnormal"
 
   sample = {}
-  for i in ks:
+  for i in ks: # Creates a sample dictionary for testing, keeping just in case it is useful later.
     sample[i] = [0, np.inf, False, 0, np.inf]
     
   @classmethod
-  def specflag(cls, klims=sample):
+  def specflag(cls, klims=sample): # Flags abnormal spectral data.
     # klims = {"k": [lowerave, upperave, cause=False, mindip, maxpeak]} 
     # Also get s11 timestamp, image of telescope, and big switchstate.
     flags = {}
     c = ""
-    for k in cls.ks:
+    for k in cls.ks: # Assumes nornal until it gores outside certain bounds.
       good = True
       ave = np.mean(np.log10(np.abs(cls.spec[k][0])))
       if klims[k][0] > ave:
@@ -243,7 +242,7 @@ class Website:
       elif klims[k][1] < ave:
         good = False
         c = k + " is too high." 
-      if klims[k][2]:
+      if klims[k][2]: # If it is not supposed to dip below certain values at all.
         for i in np.log10(np.abs(cls.spec[k][0])):
           if i < klims[k][3]:
             good = False
@@ -251,37 +250,37 @@ class Website:
           elif klims[k][4] < i:
             good = False
             c = k + " peaks above " + str(klims[k][4]) 
-      if klims[k][2]:
+      if klims[k][2]: # Format of the resulting structure.
         flags[k] = [good, c]
       else:
         flags[k] = [good, "x"]
     return flags
     
   @classmethod
-  def tempflag(cls, seconds=10):
+  def tempflag(cls, seconds=10): # Flags normal and abnormal temperatures.
     # pvals = [actrl, bctrl]
     probs = [[False, False, False,], [False, False,False,]]
     labs = [[3,"A_status", "A_T_target"], [3,"B_status", "B_T_target"]]
     for i in range(2):
       if cls.mlist[labs[i][0]][labs[i][1]] != "error":
-        if cls.tdata[i][1][-1] > cls.mlist[labs[i][0]][labs[i][2]] + 5:
+        if cls.tdata[i][1][-1] > cls.mlist[labs[i][0]][labs[i][2]] + 5: # Checks if the current value is too high.
           probs[i][0] = True
-        if cls.tdata[i][1][-1] > cls.mlist[labs[i][0]][labs[i][2]] - 5:
+        if cls.tdata[i][1][-1] > cls.mlist[labs[i][0]][labs[i][2]] - 5: # Checks if the current value is too low.
           probs[i][2] = True
         if not probs[i][0]:
-          if len(cls.tdata) > 300: 
-            x = len(cls.tdata) - 300
+          if len(cls.tdata) > seconds * 10: 
+            x = len(cls.tdata) - seconds * 10
           else: 
             x = 0
-          if np.mean(cls.tdata[i][1][x:-1]) > pvals[i] + 5:
+          if np.mean(cls.tdata[i][1][x:-1]) > pvals[i] + 5: # Checks if average is above certain value.
             probs[i][1] = True
     return probs
   
   @classmethod
-  def buildpage(cls, meta={}, data={}, cal={}, spec = {}, fname="", active=False, path="."):
+  def buildpage(cls, meta={}, data={}, cal={}, spec = {}, fname="", active=False, path="."): # Creates webpage; "active" specifices wheter it is live or static.
     # global opene
     # global IMGGGG
-    if not active:
+    if not active: # Gets data if just reading from a regular file.
       normal = activeflag(data,cal)
       mia = meta["imu_antenna"]
       mip = meta["imu_panda"]
@@ -289,33 +288,35 @@ class Website:
       lid = meta["lidar"]
       mot = meta["motor"]
       rfs = meta["rfswitch"]
-    if active:
+    if active: # Gets data if live.
       try:       
-        mia, mip, tec, lid, mot, rfs = cls.grabbe()
+        mia, mip, pot, tec, lid, mot, rfs = cls.grabbe()
       except KeyError:
         print("No metadata being collected; this is going to cause problems!")
-        mia, mip, tem, tec, lid, mot, rfs = [0], [0], ["xxx"], {"A_status": "error", "B_status": "error", "A_timestamp":0, "A_T_now":0, "B_timestamp":0, "B_T_now":0}, {"distance_m": 0}, {"az_pos": 0, "el_pos": 0}, {"sw_state":0}
-      normal = activeflag(cls.data, cls.cal)
-    else:
+        mia, mip, pot, tec, lid, mot, rfs = [0], [0], {"pot_el_voltage": 0, "pot_az_voltage":0}, {"A_status": "error", "B_status": "error", "A_timestamp":0, "A_T_now":0, "B_timestamp":0, "B_T_now":0}, {"distance_m": 0}, {"az_pos": 0, "el_pos": 0}, {"sw_state":0}
+      normal = activeflag(cls.data, cls.cal) # The table of normal values is from current data.
+    else: # If not active, updates tdata on its over; the graph will be one point in time.
       tdata = np.append(tdata, [
                               [[tec["A_timestamp"]], [tec["A_T_now"]]], [[tec["B_timestamp"]], [tec["B_T_now"]]]], axis = 2)
-    if not (mia == [0] and mip == [0]):
-      tgraph = """
+    if not (mia == [0] and mip == [0]): # If there is metadata at all, build the metadata box and temperature graph. 
+      tgraph = """ 
       <img src="data:image/png;base64,""" + cls.seetemp() + """" width="90%">
-        """
-      mtab = """
+        """ # Temperature graph.
+      mtab = """ 
       <div class="boxes" id="tool">
       <h4 style="text-align: center">Motor</h4>
       <div class="mon">
         <li style="text-align:center"><b>AZ</b></li>
         <li>Position: """ + str(mot["az_pos"]) + """</li>
+        <li>Pot. Voltage: """ + str(pot["pot_az_voltage"]) + """</li>
         <li style="text-align:center"><b>EL</b></li>
         <li>Position: """ + str(mot["el_pos"]) + """</li>
+        <li>Pot. Voltage: """ + str(pot["pot_el_voltage"]) + """</li>
       </div>
       <p>Lidar Distance: """ + str(lid["distance_m"]) + """ meters</p>
       </div>
-      """
-    else:
+      """ # Metadata tab.
+    else: # If no metadata, inform the user.
       tgraph = """
       <p>No temperature yet!</p>
         """
@@ -326,44 +327,42 @@ class Website:
       """
     terror = """"""
     s = 10
-    probs = cls.tempflag(seconds=s)
+    probs = cls.tempflag(seconds=s) # Looks for abnormalities in temperature.
     labs = ["A-control", "B-control"]
     if True:
       for i in range(len(probs)):
-        if probs[i][0]:
+        if probs[i][0]: # Most urgent warning.
           terror += """
         <h2 style="color: red;">WARNING: """ + str(labs[i]) + """ is actively overheating!!!</h2>
            """
-        elif probs[i][1]:
+        elif probs[i][1]: # Notifies if the average is too high.
           terror += """
         <p style="color: yellow;">Notice: """ + str(labs[i]) + """ approached high temperatures in the last """ + str(s) + """ seconds.</p>
            """
-        elif probs[i][2]:
+        elif probs[i][2]: # Notifies if the temperature is too low.
           terror += """
         <p style="color: yellow;">Notice: """ + str(labs[i]) + """ is five degrees below target.</p>
            """
-      for boo in ["A_status", "B_status"]:
+      for boo in ["A_status", "B_status"]: # Notifies if error in collecting temperatures.
         if tec[boo] == "error":
           terror += """
         <p>Error in control """ + boo + """</p>
             """
-    if not cls.metw:
-      terror = """"""
     if True:
-      if len(normal) == 2:
+      if len(normal) == 2: # If there's only recorded data, only report that.
         dlist = """Recording: """ + cls.good(normal["rec"]) + """</p>
       """
-      else:
+      else: # Otherwise, tell whether everything else is alright.
         dlist = """Antenna: """ + cls.good(normal["ant"]) + """, Load: """ + cls.good(normal["load"]) + """, Noise: """ + cls.good(normal["noise"]) + """</p>
       """
-    if active:
-      if cls.cal["VNAO"] == [0] and len(cls.cal) == 3:
+    if active: # If it is live, display live s11 and spectral data.
+      if cls.cal["VNAO"] == [0] and len(cls.cal) == 3: # If there is no s11 data, report.
         imtab = """
     <div class="boxes" id="s11">
       <p>No S11 data yet!</p>
     </div>
       """
-      else:
+      else: # Otherside, display graph.
         imtab = """
     <div class="boxes" id="s11">
       <img src="data:image/png;base64,""" + cls.seefile(cls.data, cls.cal) + """" width="90%">
@@ -376,25 +375,25 @@ class Website:
       # specfunc = ""
       #  
       swarning = """
-      <p style="color: red;">Bad Spectra: """
+      <p style="color: red;">Bad Spectra: """ # If there are bad spectra, report.
       normie = cls.specflag()
       for k in cls.ks:
-        if normie[k][1] != "x":
+        if normie[k][1] != "x": # If the webpage is to diagnose the bad spectra, give cause.
           swarning += normie[k][1]
-        elif not normie[k][0]:
+        elif not normie[k][0]: # If it is not, just name spectra and move on.
           if swarning[-1] != " ":
             swarning += ", "
           swarning += k
       swarning += """</p>
       """
-      if not (cls.spec["0"] == np.array([[0]]) and cls.spec["1"] == np.array([[0]])):
+      if not (cls.spec["0"] == np.array([[0]]) and cls.spec["1"] == np.array([[0]])): # If spectra exist, build tab.
         stab = """
     <div class="boxes" id="spec">
     <img id="g" class="gs" style.display="block" src="data:image/png;base64,""" + cls.seespec() + """" width="90%">
     """ + swarning + """
     </div>
     """
-      else:
+      else: # If not, inform user.
         stab = """
     <div class="boxes" id="spec">
     <p>No spectra yet!</p>
@@ -406,7 +405,7 @@ class Website:
       sbutton = """
             <button onclick="showhide('spec')">Spectrum</button>
     """
-    else:
+    else: # If not live, just display what is in file.
       imtab = """
       <div class="boxes" id="s11">
         <img src="data:image/png;base64,""" + cls.seefile(data, cal) + """" width="90%">
@@ -416,7 +415,7 @@ class Website:
       stab = ""
       specfunc = ""
       sbutton = ""
-    collecting = """"""
+    collecting = """""" # Tells user what data is actively not being collected.
     if not cls.spew:
       collecting += """
         <p style="text-align: center;">Warning: Spectra are not being collected!</p>
@@ -429,6 +428,7 @@ class Website:
       collecting += """
         <p style="text-align: center;">Warning: Metadata is not being collected!</p>
       """
+    # Begin webpage.
     html = """<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -566,26 +566,26 @@ class Website:
     """ + mtab + """
     </div>
 </body>
-</html>"""
+</html>""" # End webpage contruction.
     fiel = ""
-    if fname == "":
+    if fname == "": # If live or no filename given, then give a recognizable name.
       fiel = "demo" + str(np.random.randint(1, 1000000)) + ".html"
       fiel = "thisone4986349238648392.html"
     else:
       fiel = cls.ripper(fname) + ".html"
-    with open(fiel, "w") as f:
+    with open(fiel, "w") as f: # Opens up a page automatically, if we figure out how.
       f.write(html)
       if not cls.opene:
         #subprocess.call(["open", "thisone4986349238648392.html"], shell=True)
         cls.opene = True
 
   @classmethod
-  def check(cls):
+  def check(cls): # A function helpful for debugging.
     print("Metadata for page: \n" + str(cls.grabbe()))
     print("Temperature: \n" + str(cls.tdata))
   
   @classmethod
-  def foldersite(cls, s11folder, path="~/EIGSEP-Flagger"): ## Will evolve.
+  def foldersite(cls, s11folder, path="~/EIGSEP-Flagger"): # Goes through each file in a folder to build a static/nonlive webpage.
     opened = False
     for path, folders, files in os.walk(s11folder):
       for fname in files:
@@ -598,9 +598,9 @@ class Website:
 
 #------
 w = Website()
-spthread = threading.Thread(target=w.seespectrum, args=(Website.ks,), daemon=True)
-methread = threading.Thread(target=w.grabbit, args=(), daemon=True)
-s11thread = threading.Thread(target=w.grabs11, args=(), daemon=True)
+spthread = threading.Thread(target=w.seespectrum, args=(Website.ks,), daemon=True) # Collects spectra.
+methread = threading.Thread(target=w.grabbit, args=(), daemon=True) # Collects metadata.
+s11thread = threading.Thread(target=w.grabs11, args=(), daemon=True) # Collects s11 data.
 spthread.start()
 methread.start()
 s11thread.start()
@@ -613,7 +613,7 @@ s11thread.start()
 #     THREADS[k] = threading.Thread(target=w.secspec, args=(k,), daemon=True)
 #   for k in THREADS:
 #     THREADS[k].start()
-while True:
+while True: # Rebuilds and refresses webpage every two seconds.
     try:
       w.buildpage(active=True)
       #print("Webpage refreshed " + str(x) + " times.")
